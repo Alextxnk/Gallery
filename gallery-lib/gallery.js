814ed07d9f8546d51b880073d2dad2eee863e0f2
Alextxnk <alexsolov766@gmail.com>
2022-08-12 18:54:07 +0300

    first lesson

--- gallery-lib/gallery.js
+++ gallery-lib/gallery.js
@@ -5,6 +5,12 @@ const GalleryClassName = 'gallery';
 const GalleryDraggableClassName = 'gallery-draggable';
 const GalleryLineClassName = 'gallery-line';
 const GallerySlideClassName = 'gallery-slide';
+const GalleryDotsClassName = 'gallery-dots'; // все точки (контейнер наших точек)
+const GalleryDotClassName = 'gallery-dot'; // точка
+const GalleryDotActiveClassName = 'gallery-dot-active'; // активная точка
+const GalleryNavClassName = 'gallery-nav'; // стрелки 
+const GalleryNavLeftClassName = 'gallery-nav-left'; // левая стрелка
+const GalleryNavRightClassName = 'gallery-nav-right'; // правая стрелка
 
 // т.к. галерея может быть на странице не одна, а множество, то удобнее всего написать библиотеку через класс  
 // 1. создаем класс Gallery 
@@ -30,6 +36,12 @@ class Gallery {
       this.stopDrag = this.stopDrag.bind(this);
       this.dragging = this.dragging.bind(this);
       this.setStylePosiion = this.setStylePosiion.bind(this);
+      this.clickDots = this.clickDots.bind(this);
+      this.moveToLeft = this.moveToLeft.bind(this);
+      this.moveToRight = this.moveToRight.bind(this);
+      this.changeCurrentSlide = this.changeCurrentSlide.bind(this);
+      this.changeActiveDotClass = this.changeActiveDotClass.bind(this);
+
 
       // вызвываем функции
       this.manageHTML();
@@ -45,9 +57,16 @@ class Gallery {
          <div class="${GalleryLineClassName}">
             ${this.containerNode.innerHTML}
          </div>
+         <div class="${GalleryNavClassName}">
+            <button class="${GalleryNavLeftClassName}">Left</button>
+            <button class="${GalleryNavRightClassName}">Right</button>
+         </div>
+         <div class="${GalleryDotsClassName}"></div>
       `;
       // получаем объект по классу gallery-line
       this.lineNode = this.containerNode.querySelector(`.${GalleryLineClassName}`);
+      // получаем объект по классу gallery-dots
+      this.dotNode = this.containerNode.querySelector(`.${GalleryDotsClassName}`);
 
       this.slideNodes = Array.from(this.lineNode.children).map((childNode) =>
          wrapElementByDiv({
@@ -55,6 +74,16 @@ class Gallery {
             className: GallerySlideClassName
          })
       );
+
+      // теперь нужно добавить необходимое количество точек, 
+      // this.size - свойство, которое определяет количество слайдов  
+      this.dotNode.innerHTML = Array.from(Array(this.size).keys()).map((key) => (
+         `<button class="${GalleryDotClassName} ${key === this.currentSlide ? GalleryDotActiveClassName : ''}"></button>`
+      )).join('');
+
+      this.dotNodes = this.dotNode.querySelectorAll(`.${GalleryDotClassName}`); // точки 
+      this.navLeft = this.containerNode.querySelector(`.${GalleryNavLeftClassName}`); // левая стрелка
+      this.navRight = this.containerNode.querySelector(`.${GalleryNavRightClassName}`); // правая стрелка
    }
 
    setParameters() {
@@ -78,21 +107,34 @@ class Gallery {
       });
    }
 
+   // добавляем события
    setEvents() {
       // меняем размер в зависимости от пользовательского размера 
       // добавляем функцию-хелпер debounce, чтоб слишком часто не вызывалась функция setEvents
       this.debouncedResizeGallery = debounce(this.resizeGallery);
+
       window.addEventListener('resize', this.debouncedResizeGallery); 
       this.lineNode.addEventListener('pointerdown', this.startDrag);
       window.addEventListener('pointerup', this.stopDrag);
       window.addEventListener('pointercancel', this.stopDrag);
+
+      // для dotNode мы будем использовать дилегирование событий,
+      // будем кликать по контейнеру ноды и определять уже нашу точку 
+      this.dotNode.addEventListener('click', this.clickDots); // клик по точке
+      this.navLeft.addEventListener('click', this.moveToLeft); // клик по левой стрелке
+      this.navRight.addEventListener('click', this.moveToRight); // клик по правой стрелке
    }
 
+   // удаляем события
    destroyEvents() {
       window.removeEventListener('resize', this.debouncedResizeGallery);
       this.lineNode.removeEventListener('pointerdown', this.startDrag);
       window.removeEventListener('pointerup', this.stopDrag);
       window.removeEventListener('pointercancel', this.stopDrag);
+
+      this.dotNode.removeEventListener('click', this.clickDots); 
+      this.navLeft.removeEventListener('click', this.moveToLeft); 
+      this.navRight.removeEventListener('click', this.moveToRight);
    }
 
    resizeGallery() {
@@ -112,9 +154,7 @@ class Gallery {
    stopDrag() {
       window.removeEventListener('pointermove', this.dragging);
       this.containerNode.classList.remove(GalleryDraggableClassName); // удаляем класс 
-      this.x = -this.currentSlide * (this.width + this.settings.margin);
-      this.setStylePosiion();
-      this.setStyleTransition(); 
+      this.changeCurrentSlide(); // вызов функции 
    }
 
    dragging(evt) {
@@ -149,14 +189,77 @@ class Gallery {
       }
    }
 
+   // клик по точке
+   clickDots(evt) {
+      // метод closest для поиска близжайшего родителя по переданному селектору button
+      const dotNode = evt.target.closest('button');
+      if (!dotNode) {
+         return; 
+      }
+
+      // определяем порядковый номер точки, по которой кликнули 
+      let dotNumber;
+      for(let i =0; i < this.dotNodes.length; i++) {
+         if (this.dotNodes[i] === dotNode) {
+            dotNumber = i;
+            break;
+         }
+      }
+
+      if (dotNumber === this.currentSlide) {
+         return;
+      }
+
+      const countSwipes = Math.abs(this.currentSlide - dotNumber); // берем модуль 
+      this.currentSlide = dotNumber;
+      this.changeCurrentSlide(countSwipes);
+   }
+
+   // клик по левой стрелке
+   moveToLeft() {
+      if (this.currentSlide <= 0) {
+         return;
+      }
+
+      this.currentSlide = this.currentSlide - 1;
+      this.changeCurrentSlide();
+   }
+
+   // клик по правой стрелке
+   moveToRight() {
+      if (this.currentSlide >= this.size - 1) {
+         return;
+      }
+
+      this.currentSlide = this.currentSlide + 1;
+      this.changeCurrentSlide();
+   }
+
+   // смена слайда 
+   changeCurrentSlide(countSwipes) {
+      this.x = -this.currentSlide * (this.width + this.settings.margin);
+      this.setStylePosiion();
+      this.setStyleTransition(countSwipes); 
+      this.changeActiveDotClass(); 
+   }
+
+   // смена активного класса у слайда 
+   changeActiveDotClass() {
+      for(let i = 0; i < this.dotNodes.length; i++) {
+         this.dotNodes[i].classList.remove(GalleryDotActiveClassName); // удаляем активный класс
+      }
+
+      this.dotNodes[this.currentSlide].classList.add(GalleryDotActiveClassName); // добавляем активный класс
+   }
+
    // смена слайдов 
    setStylePosiion() {
       this.lineNode.style.transform = `translate3d(${this.x}px, 0, 0)`;
    }
 
    // подключаем анимацию, чтоб плавно сменялись слайды 
-   setStyleTransition() {
-      this.lineNode.style.transition = `all 0.25s ease 0s`;
+   setStyleTransition(countSwipes = 1) {
+      this.lineNode.style.transition = `all ${0.25 * countSwipes}s ease 0s`;
    }
 
    // сброс стиля анимации, когда перешли на новый слайд 
